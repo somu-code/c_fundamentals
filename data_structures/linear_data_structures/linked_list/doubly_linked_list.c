8b92ab2011fca5800aa7d0251073d8ce14f78796
Somnath Golui <code.somu@gmail.com>
2026-06-22 16:00:08 +0530

    Wrote print_list and insert_at_beginning functions

--- data_structures/linear_data_structures/linked_list/doubly_linked_list.c
+++ data_structures/linear_data_structures/linked_list/doubly_linked_list.c
@@ -1,3 +1,6 @@
+#include <stdio.h>
+#include <stdlib.h>
+
 struct Node {
 	struct Node *prev;
 	int data;
@@ -5,9 +8,26 @@ struct Node {
 };
 
 struct Node *create_node(int data);
+void print_list(struct Node *head);
+void insert_at_beginning(struct Node **head, struct Node *new_node);
 
 int main(void)
 {
+	struct Node *head = NULL;
+	head = create_node(0);
+	if (head == NULL) {
+		return EXIT_FAILURE;
+	}
+	/*struct Node *second_node = create_node(2);
+	if (second_node == NULL) {
+		return EXIT_FAILURE;
+	}
+	head->next = second_node;
+	second_node->prev = head;*/
+	print_list(head);
+	insert_at_beginning(&head, create_node(100));
+	printf("After inserting a node at begining with value :100\n");
+	print_list(head);
 }
 
 struct Node *create_node(int data)
@@ -21,4 +41,33 @@ struct Node *create_node(int data)
 	temp->data = data;
 	temp->next = NULL;
 	return temp;
+}
+
+void print_list(struct Node *head)
+{
+	if (head == NULL) {
+		return;
+	} else if (head->next == NULL) {
+		printf("prev: %p ", (void *)head->prev);
+		printf("data: %d ", head->data);
+		printf("next: %p\n", (void *)head->next);
+	} else {
+		while (head != NULL) {
+			printf("prev: %p ", (void *)head->prev);
+			printf("data: %d ", head->data);
+			printf("next: %p\n", (void *)head->next);
+			head = head->next;
+		}
+	}
+}
+
+void insert_at_beginning(struct Node **head, struct Node *new_node)
+{
+	if (*head == NULL) {
+		*head = new_node;
+	} else {
+		new_node->next = *head;
+		(*head)->prev = new_node;
+		*head = new_node;
+	}
 }
