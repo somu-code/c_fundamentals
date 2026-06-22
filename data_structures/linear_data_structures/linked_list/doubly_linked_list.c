baccf17735c14b6e35cdf002766399fa52bfab3b
Somnath Golui <code.somu@gmail.com>
2026-06-22 23:20:05 +0530

    Added a tail poiner

--- data_structures/linear_data_structures/linked_list/doubly_linked_list.c
+++ data_structures/linear_data_structures/linked_list/doubly_linked_list.c
@@ -9,25 +9,32 @@ struct Node {
 
 struct Node *create_node(int data);
 void print_list(struct Node *head);
-void insert_at_beginning(struct Node **head, struct Node *new_node);
+void insert_at_beginning(struct Node **head, struct Node **tail,
+			 struct Node *new_node);
+void insert_at_end(struct Node *tail, struct Node *new_node);
 
 int main(void)
 {
 	struct Node *head = NULL;
+	struct Node *tail = NULL;
 	head = create_node(0);
 	if (head == NULL) {
 		return EXIT_FAILURE;
 	}
-	/*struct Node *second_node = create_node(2);
+	tail = head;
+	struct Node *second_node = create_node(2);
 	if (second_node == NULL) {
 		return EXIT_FAILURE;
 	}
 	head->next = second_node;
-	second_node->prev = head;*/
+	second_node->prev = head;
+	tail = second_node;
+	printf("Node list:\n");
 	print_list(head);
-	insert_at_beginning(&head, create_node(100));
-	printf("After inserting a node at begining with value :100\n");
+	insert_at_beginning(&head, &tail, create_node(100));
+	printf("\nAfter inserting a node at begining with value :100\n");
 	print_list(head);
+	printf("\nValue stored in tail: %d\n", tail->data);
 }
 
 struct Node *create_node(int data)
@@ -61,10 +68,12 @@ void print_list(struct Node *head)
 	}
 }
 
-void insert_at_beginning(struct Node **head, struct Node *new_node)
+void insert_at_beginning(struct Node **head, struct Node **tail,
+			 struct Node *new_node)
 {
 	if (*head == NULL) {
 		*head = new_node;
+		*tail = *head;
 	} else {
 		new_node->next = *head;
 		(*head)->prev = new_node;
